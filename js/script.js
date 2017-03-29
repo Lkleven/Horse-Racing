a4a87506463b7050d1251e45e288f30bf7d8cf10
Lars Ivar Kleven <likleven@gmail.com>
2017-03-29 18:35:34 +0200

    Changed display of results
    Results are now displayed in order of finish rather than fixed.

--- js/script.js
+++ js/script.js
@@ -9,10 +9,13 @@ $(function  () {
 		var positionsArray = ["first", "second", "third", "fourth"];
 		var position = 0;
 		var place ="";
+		var raceInf = "raceInfo";
+		var colors =["#019943", "#f29e02", "#ff023d", "#3b83ff"];	//green, orange, red, blue
 
 		function checkPositionAtFinish(){
 			place = positionsArray[position];
 			position++;
+			raceInf = raceInf.slice(0,8) + position;
 
 			//Enables the reset button once all horses have finished
 			if(position == positionsArray.length){
@@ -36,7 +39,8 @@ $(function  () {
 			left: racetrackWidth
 		}, raceTime1, function(){
 			checkPositionAtFinish();
-			$("#raceInfo1 span").text("Finished in " + place + " place and clocked in at " + raceTime1 + " milliseconds!");
+			$("#"+ raceInf + " em").text("Green Horse") .css("color", colors[0]);
+			$("#"+ raceInf + " span").text("Finished in " + place + " place and clocked in at " + raceTime1 + " milliseconds!");
 		});
 
 		
@@ -47,7 +51,9 @@ $(function  () {
 			left: racetrackWidth
 		}, raceTime2, function(){
 			checkPositionAtFinish();
-			$("#raceInfo2 span").text("Finished in " + place + " place and clocked in at " + raceTime2 + " milliseconds!");
+			
+			$("#"+ raceInf + " em").text("Orange Horse") .css("color", colors[1]);
+			$("#"+ raceInf + " span").text("Finished in " + place + " place and clocked in at " + raceTime2 + " milliseconds!");
 		});
 
 		
@@ -58,7 +64,8 @@ $(function  () {
 			left: racetrackWidth
 		}, raceTime3, function(){
 			checkPositionAtFinish();
-			$("#raceInfo3 span").text("Finished in " + place + " place and clocked in at " + raceTime3 + " milliseconds!");
+			$("#"+ raceInf + " em").text("Red Horse") .css("color", colors[2]);
+			$("#"+ raceInf + " span").text("Finished in " + place + " place and clocked in at " + raceTime3 + " milliseconds!");
 		});
 
 		
@@ -70,7 +77,8 @@ $(function  () {
 			left: racetrackWidth
 		}, raceTime4, function(){
 			checkPositionAtFinish();
-			$("#raceInfo4 span").text("Finished in " + place + " place and clocked in at " + raceTime4 + " milliseconds!");
+			$("#"+ raceInf + " em").text("Blue Horse") .css("color", colors[3]);
+			$("#"+ raceInf + " span").text("Finished in " + place + " place and clocked in at " + raceTime4 + " milliseconds!");
 		});
 
 		
@@ -79,6 +87,7 @@ $(function  () {
 		$("#reset").click(function(){
 			$(".horse").css("left", "0");
 			$(".raceInfo span").text("");
+			$(".raceInfo em").text("");
 			$("#go").attr("disabled", false).removeClass("disabled");
 			$(this).attr("disabled", true).addClass("disabled");
 		})
